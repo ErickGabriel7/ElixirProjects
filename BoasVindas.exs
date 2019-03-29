a50e01884ab2c964e5932b926c39ba8c29f5dc0b
ErickGabriel7 <46802935+ErickGabriel7@users.noreply.github.com>
2019-03-29 02:10:54 -0300

    Delete BoasVindas.exs

--- BoasVindas.exs
+++
@@ -1,7 +0,0 @@
-bv = IO.gets "Ola! Digite uma velocidade em km/h\n>>>"
-bv = String.trim(bv)
-bv = String.to_integer(bv) #Quando coloquei String.to_float(bv) deu erro.
-convert = fn(v) ->
-  v/3.6
-end
-IO.puts("#{bv} km/h em m/s e: #{convert.(bv)}")
